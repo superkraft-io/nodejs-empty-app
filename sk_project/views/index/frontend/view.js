38bfb530193ef13b7dd6929d1021698a2a467185
Aid Vllasaliu <aid@superkraft.io>
2024-05-05 17:46:45 +0200

    0.3.1
    - Added some core components. More will be added.

--- sk_project/views/index/frontend/view.js
+++ sk_project/views/index/frontend/view.js
@@ -3,7 +3,61 @@ class SK_App_View extends sk_ui_component {
         super(opt)
 
         this.add.label(_c => {
-            _c.text = 'hello :)'
+            _c.text = 'Hello :)'
         })
+
+        this.add.checkbox(_c => {
+            _c.styling = 'top left'
+            _c.text = 'Click me'
+        })
+
+        this.add.checkmark(_c => {
+            _c.styling = ''
+            _c.size = 32
+            setTimeout(()=>{ _c.check() }, 3000)
+        })
+
+        this.add.colorPicker(_c => {
+        
+        })
+
+        this.add.dropdown(_c => {
+            _c.text = 'Dropdown'
+            _c.items = [
+                {header: true, label: 'This is a header'},
+                {label: 'Item 1'},
+                {label: 'Item 2', icon: 'dog'},
+                {separator: true},
+                {label: 'Submenu', items: [
+                    {label: 'Submenu item'}
+                ]}
+
+            ]
+        })
+
+        
+        this.add.group(_c => {
+            _c.header = 'Group'
+            _c.container.setup(_c => {
+                _c.add.button(_c => {
+                    _c.text = 'button inside a group'
+                })
+            })
+        })
+
+        this.add.groupCollapsable(_c => {
+            _c.header = 'Group Collapsable'
+            _c.container.setup(_c => {
+                _c.add.button(_c => {
+                    _c.text = 'button inside a collapsable group'
+                })
+            })
+        })
+        
+
+        this.add.icon(_c => {
+            _c.icon = 'dog'
+        })
+        
     }
 }
