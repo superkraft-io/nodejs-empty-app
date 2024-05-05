9d00f2820632c4d3131a731c4794e85ad7336533
Aid Vllasaliu <aid@superkraft.io>
2024-05-05 18:05:51 +0200

    0.3.2
    - More core UI components added. Even more will be added later.

--- sk_project/views/index/frontend/view.js
+++ sk_project/views/index/frontend/view.js
@@ -59,5 +59,58 @@ class SK_App_View extends sk_ui_component {
             _c.icon = 'dog'
         })
         
+        this.add.infoLabel(_c => {
+            _c.icon = 'dog'
+            _c.text = 'This is an infolabel with an icon'
+            _c.size = 14
+        })
+
+        this.add.input(_c => {
+            _c.placeholder = 'This is an input'
+            _c.onChanged = res => {
+                console.log(res)
+            }
+        })
+
+        this.add.labelledSwitch(_c => {
+            _c.labels.left.text = 'OFF'
+            _c.labels.right.text = 'ON'
+        })
+
+        this.add.linkButton(_c => {
+            _c.icon = 'dog'
+            _c.text = 'This is a link button'
+
+            //_c.goto will open URL in the same window
+            //_c.goto_ will open URL in a new window
+            _c.goto_ = 'https://github.com/superkraft-io/superkraft'
+        })
+
+        this.add.loader(_c => {
+        })
+
+        this.add.separator(_c => {
+            _c.width = 256
+        })
+
+        this.add.slider(_c => {
+            _c.width = 256
+            _c.min = 0
+            _c.max = 100
+            //_c.smooth = true
+        })
+
+
+        this.add.text(_c => {
+            _c.width = 128
+            _c.wrap = true
+            _c.text = 'This is a multi-line:able text to allow for multi-line text content'
+        })
+
+        this.add.textarea(_c => {
+            _c.width = 128
+            _c.wrap = true
+            _c.value = 'This is a multi-line:able text area to allow for multi-line text input'
+        })
     }
 }
